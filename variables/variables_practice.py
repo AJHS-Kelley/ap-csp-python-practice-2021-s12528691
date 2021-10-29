c5a4480eb73fbd6f831d18ffbf7154bbba894af8
s12528691 <70608521+s12528691@users.noreply.github.com>
2021-10-29 13:03:54 -0400

    version 0.1
    Added three variables
    int.
    float
    string.

---
+++ variables/variables_practice.py
@@ -0,0 +1,8 @@
+# Vriables Practice, Sacha Johnson, 10/29/21 1:01pm, version 0.1
+
+
+# Basic Variables
+
+my_int = -11 # INTEGERS, any +/- whole number and 0.
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
+my_string = "Live Love Laugh"
